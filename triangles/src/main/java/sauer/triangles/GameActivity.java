ed91a6dd192802c88a1d3f40ee45bbe259ffc746
Fred Sauer <fredsa@google.com>
2014-06-14 14:32:39 -0700

    First visible triangle

---
+++ triangles/src/main/java/sauer/triangles/GameActivity.java
@@ -0,0 +1,29 @@
+package sauer.triangles;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class GameActivity extends Activity {
+
+
+    private GLSurfaceView mGLView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        mGLView = new MyGLSurfaceView(this);
+        setContentView(mGLView);
+    }
+}
